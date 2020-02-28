35c9774bf395cd18d94f41fba2689f77535150b8
Secil Tuncay <32872209+SecilTuncay@users.noreply.github.com>
2020-02-28 11:12:59 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Todolist from './containers/Todolist';
 class App extends Component {
   render() {
     return (
-      <div className="App" >
+      <div className="App">
         <header className="App-header">
-          FIRST APP
+          TODOLIST
         </header>
         <Todolist />
       </div>
